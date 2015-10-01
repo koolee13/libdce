5c45f781d307f799af9dceb5ff636d5477694e16
Buddy Liong <a0270631@ti.com>
2015-10-01 12:43:00 -0500

    [DCE_TEST] Fixing the decoded output resolution
    Previously, when decoding for example H.264 1080p (1920x1080),
    the output yuv comes out to be 1920x1088 because codec requires
    the maxWidth and maxHeight on the VIDDEC3_Params to be aligned by 16 bytes.
    
    Problem is in dce_test.c, the aligned is done early to the global variable
    width and height which altered the value to be aligned by 16 bytes.
    On the example above the height becomes 1088 (aligned by 16 bytes).
    When the output buffer is allocated for TILER, the
    outBufs->descs[0].tileMem.width = width that is aligned to 16 bytes.
    outBufs->descs[0].tileMem.height = height that is aligned to 16 bytes.
    or when the output buffer is allocated for Non-TILER, the
    outBufs->descs[0].bytes = width * height that is aligned to 16 bytes.
    
    With this fix, the output is now corrected to have the same resolution
    as the input.
    Eg. H.264 1080p (1920x1080) will be decoded to YUV NV12 of 1920x1080.
    No longer YUV NV12 of 1920x1088.
    
    Change-Id: Icdce8dfb99ffd2568ad318ff30305f83901a8243
    Signed-off-by: Buddy Liong <a0270631@ti.com>

--- test_qnx/dce_test/dce_test.c
+++ test_qnx/dce_test/dce_test.c
@@ -725,10 +725,6 @@ int main(int argc, char * *argv)
 
     DEBUG("Num Frames is %d width=%d, height=%d", frameCount, width, height);
 
-    /* calculate output buffer parameters: */
-    width  = ALIGN2(width, 4);         /* round up to MB */
-    height = ALIGN2(height, 4);        /* round up to MB */
-
     switch( codec_switch ) {
         case DCE_TEST_H264 :
             padded_width  = ALIGN2(width + (2 * PADX_H264), 7);
@@ -792,7 +788,7 @@ int main(int argc, char * *argv)
             params->maxBitRate      = 10000000;
             params->displayDelay    = IVIDDEC3_DISPLAY_DELAY_AUTO;
             params->numOutputDataUnits  = 0;
-            params->maxWidth            = width;
+            params->maxWidth            = ALIGN2(width, 4);
             break;
         case DCE_TEST_MPEG4 :
             params = dce_alloc(sizeof(IMPEG4VDEC_Params));
@@ -804,7 +800,7 @@ int main(int argc, char * *argv)
             params->maxBitRate      = 10000000;
             params->displayDelay    = IVIDDEC3_DISPLAY_DELAY_1;
             params->numOutputDataUnits  = 0;
-            params->maxWidth            = width;
+            params->maxWidth            = ALIGN2(width, 4);
             break;
         case DCE_TEST_VC1SMP :
         case DCE_TEST_VC1AP :
@@ -817,7 +813,7 @@ int main(int argc, char * *argv)
             params->maxBitRate      = 45000000;
             params->displayDelay    = IVIDDEC3_DISPLAY_DELAY_1;
             params->numOutputDataUnits  = 0;
-            params->maxWidth            = width;
+            params->maxWidth            = ALIGN2(width, 4);
             break;
         case DCE_TEST_MJPEG :
             params = dce_alloc(sizeof(IJPEGVDEC_Params));
@@ -829,7 +825,7 @@ int main(int argc, char * *argv)
             params->maxBitRate      = 10000000;
             params->displayDelay    = IVIDDEC3_DISPLAY_DELAY_1;
             params->numOutputDataUnits  = 1;
-            params->maxWidth            = width;
+            params->maxWidth            = ALIGN2(width, 4);
             break;
 
         case DCE_TEST_MPEG2 :
@@ -847,7 +843,7 @@ int main(int argc, char * *argv)
 
     }
 
-    params->maxHeight           = height;
+    params->maxHeight           = ALIGN2(height, 4);
     params->maxFrameRate        = 30000;
     params->dataEndianness      = XDM_BYTE;
     params->forceChromaFormat   = XDM_YUV_420SP;
