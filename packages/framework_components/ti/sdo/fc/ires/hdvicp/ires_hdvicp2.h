f72096090e3063e85ea77cae66b2d99a48d91727
Buddy Liong <a0270631@ti.com>
2017-09-29 15:28:05 -0500

    [LIBDCE] Update dependency header files
    Update the header files to match with
    framework_components_3_40_02_07
    xdctools_3_50_03_33_core
    
    Modify the README on the version as well.
    The codec header files have been checked to be similar
    (only spacing different) so no update is needed.
    
    Change-Id: Ie0afb59ec58f88fd18d49d688f958cf3aafc3848
    Signed-off-by: Buddy Liong <a0270631@ti.com>

--- packages/framework_components/ti/sdo/fc/ires/hdvicp/ires_hdvicp2.h
+++ packages/framework_components/ti/sdo/fc/ires/hdvicp/ires_hdvicp2.h
@@ -1,9 +1,4 @@
 /*
- *  Copyright 2013 by Texas Instruments Incorporated.
- *
- */
-
-/*
  * Copyright (c) 2012 - 2013, Texas Instruments Incorporated
  * All rights reserved.
  *
@@ -137,7 +132,7 @@ extern "C" {
 /**
  * @brief   Protocol Revision for this resource
  */
-static IRES_ProtocolRevision    IRES_HDVICP2_PROTOCOLREVISION = { 1, 0, 0 };
+static IRES_ProtocolRevision IRES_HDVICP2_PROTOCOLREVISION = {1, 0, 0};
 
 #endif
 
@@ -151,20 +146,20 @@ typedef struct IRES_HDVICP2_Obj *IRES_HDVICP2_Handle;
  *  @brief      Timeout value to be used by implementation of
  *              IRES_HDVICP2_Handle:wait function
  */
-extern unsigned int    ti_sdo_fc_ires_hdvicp_HDVICP2_timeoutVal;
+extern unsigned int ti_sdo_fc_ires_hdvicp_HDVICP2_timeoutVal;
 
 /**
  *  @brief
  */
-typedef IRES_YieldArgs (*ti_sdo_fc_ires_hdvicp_HDVICP2_GetYieldArgsFxn)
-    (Int scratchId);
+typedef IRES_YieldArgs (* ti_sdo_fc_ires_hdvicp_HDVICP2_GetYieldArgsFxn)
+        (Int scratchId);
 
 /**
  *  @brief
  */
-typedef Void (*ti_sdo_fc_ires_hdvicp_HDVICP2_YieldFxn)
-    (IRES_YieldResourceType resource, IRES_YieldContextHandle ctxt,
-    IRES_YieldArgs args);
+typedef Void (* ti_sdo_fc_ires_hdvicp_HDVICP2_YieldFxn)
+        (IRES_YieldResourceType resource, IRES_YieldContextHandle ctxt,
+        IRES_YieldArgs args);
 
 
 
@@ -174,7 +169,7 @@ typedef Void (*ti_sdo_fc_ires_hdvicp_HDVICP2_YieldFxn)
 typedef struct IRES_HDVICP2_ProtocolArgs {
 
     IRES_ProtocolArgs base;     /**< Base protocol Args */
-    XDAS_Int32        id;       /**< HDVICP2 resource being requested.
+    XDAS_Int32 id;              /**< HDVICP2 resource being requested.
                                  *
                                  *   @remarks   Could be 0, 1, 2,
                                  *              IRES_HDVICP2_ID_ANY or
@@ -186,7 +181,7 @@ typedef struct IRES_HDVICP2_ProtocolArgs {
  *  @brief          Callback function signature, used by the IRES_HDVICP2_
  *                  Handle's configure function
  */
-typedef void (*IRES_HDVICP2_CallbackFxn)(IALG_Handle handle, Void *args);
+typedef void (*IRES_HDVICP2_CallbackFxn)(IALG_Handle handle, Void * args);
 
 /**
  *  @brief          Describes the status/history of the HDVICP2 unit that is
@@ -211,8 +206,8 @@ typedef enum IRES_HDVICP2_Status {
  */
 typedef struct  IRES_HDVICP2_Obj {
 
-    IRES_Obj   ires;                   /**< Base IRES_Obj struct */
-    Void      *info;                   /**< Reserved for future use */
+    IRES_Obj ires;                     /**< Base IRES_Obj struct */
+    Void *info;                        /**< Reserved for future use */
     XDAS_Int32 id;                     /**< HDVICP2 id granted
                                         *
                                         *  @remark  Could be 0, 1, 2, or
@@ -230,114 +225,114 @@ typedef struct  IRES_HDVICP2_Obj {
                                         *               bit 1 -> ICONT2 reset
                                         */
 
-    /**
-     *  @brief          Acquire the HDVICP2 resource.
-     *
-     *  @remarks        This call acquires the HDVICP resource which the alg may
-     *                  (or may not) have specifically requested.
-     *
-     *  @remarks        The returned @c hdvicp2Status indicates the status of
-     *                  the particular HDVICP2 unit that is returned.
-     *                  @sa IRES_HDVICP2_Status
-     *
-     *  @remarks        The @c configurationId argument is a codec specified
-     *                  parameter that is both an input and output parameter.  It
-     *                  specifies the mode in which this particular HDVICP2 was
-     *                  used last time, and the mode in which it will be
-     *                  used this time.
-     *
-     *  @remarks        This API takes a @c yieldContext as argument, as it may
-     *                  yield the processor to higher priority codecs in the
-     *                  system.
-     *
-     *  @remarks        @c lateAcquireId is an optionally framework supplied
-     *                  parameter (sometimes provided via an XDM control() call
-     *                  setting the XDM_SETLATEACQUIREARG. If not supplied, the
-     *                  codec should pass the value
-     *                  #IRES_HDVICP2_UNKNOWNLATEACQUIREARG.
-     *
-     *  @sa         IRES_HDVICP2_UNKNOWNLATEACQUIREARG
-     */
+/**
+ *  @brief          Acquire the HDVICP2 resource.
+ *
+ *  @remarks        This call acquires the HDVICP resource which the alg may
+ *                  (or may not) have specifically requested.
+ *
+ *  @remarks        The returned @c hdvicp2Status indicates the status of
+ *                  the particular HDVICP2 unit that is returned.
+ *                  @sa IRES_HDVICP2_Status
+ *
+ *  @remarks        The @c configurationId argument is a codec specified
+ *                  parameter that is both an input and output parameter.  It
+ *                  specifies the mode in which this particular HDVICP2 was
+ *                  used last time, and the mode in which it will be
+ *                  used this time.
+ *
+ *  @remarks        This API takes a @c yieldContext as argument, as it may
+ *                  yield the processor to higher priority codecs in the
+ *                  system.
+ *
+ *  @remarks        @c lateAcquireId is an optionally framework supplied
+ *                  parameter (sometimes provided via an XDM control() call
+ *                  setting the XDM_SETLATEACQUIREARG. If not supplied, the
+ *                  codec should pass the value
+ *                  #IRES_HDVICP2_UNKNOWNLATEACQUIREARG.
+ *
+ *  @sa         IRES_HDVICP2_UNKNOWNLATEACQUIREARG
+ */
     XDAS_Void (*acquire)(IALG_Handle algHandle,
-                         struct IRES_HDVICP2_Obj *hdvicpHandle,
-                         IRES_YieldContext *yieldCtxt, XDAS_UInt32 *hdvicp2Status,
-                         XDAS_UInt32 *configurationId, XDAS_Int32 lateAcquireArg);
+            struct IRES_HDVICP2_Obj * hdvicpHandle,
+            IRES_YieldContext * yieldCtxt, XDAS_UInt32 * hdvicp2Status,
+            XDAS_UInt32 *configurationId, XDAS_Int32 lateAcquireArg);
 
-    /**
-     *  @brief      Release the HDVICP2 resource.
-     *
-     *  @remarks    This makes the resource available for use by other algorithms.
-     */
+/**
+ *  @brief      Release the HDVICP2 resource.
+ *
+ *  @remarks    This makes the resource available for use by other algorithms.
+ */
     XDAS_Void (*release)(IALG_Handle algHandle,
-                         struct IRES_HDVICP2_Obj *hdvicpHandle);
+            struct IRES_HDVICP2_Obj * hdvicpHandle);
 
-    /**
-     *  @brief      Reacquire the HDVICP2 resource recent released.
-     *
-     *  @returns    XDAS_TRUE       The resource was not yet acquired by another
-     *                              algorithm, and the resource handle is still
-     *                              valid.
-     *  @returns    XDAS_FALSE      The resource is no longer available and has been
-     *                              deactivated.
-     */
+/**
+ *  @brief      Reacquire the HDVICP2 resource recent released.
+ *
+ *  @returns    XDAS_TRUE       The resource was not yet acquired by another
+ *                              algorithm, and the resource handle is still
+ *                              valid.
+ *  @returns    XDAS_FALSE      The resource is no longer available and has been
+ *                              deactivated.
+ */
     XDAS_UInt32 (*reacquireIfOwner)(IALG_Handle algHandle,
-                                    struct IRES_HDVICP2_Obj *hdvicpHandle);
+            struct IRES_HDVICP2_Obj * hdvicpHandle);
 
-    /**
-     *  @brief      Register a callback function with a HDVICP2 instance.
-     *
-     *  @remarks    The callback is invoked when an interrupt fires after
-     *              processing data.
-     *
-     *  @remarks    It is the algorithm's responsibility to configure the HDVICP2
-     *              to process data and fire the interrupt when done.
-     */
+/**
+ *  @brief      Register a callback function with a HDVICP2 instance.
+ *
+ *  @remarks    The callback is invoked when an interrupt fires after
+ *              processing data.
+ *
+ *  @remarks    It is the algorithm's responsibility to configure the HDVICP2
+ *              to process data and fire the interrupt when done.
+ */
     XDAS_Void (*configure)(IALG_Handle algHandle,
-                           struct IRES_HDVICP2_Obj *hdvicpHandle,
-                           IRES_HDVICP2_CallbackFxn cbFunctionPtr, XDAS_Void *cbArgs);
+            struct IRES_HDVICP2_Obj * hdvicpHandle,
+            IRES_HDVICP2_CallbackFxn cbFunctionPtr, XDAS_Void * cbArgs);
 
-    /**
-     *  @brief      Wait until the HDVICP2 is done processing and then return.
-     *
-     *  @remarks    This is a blocking call, unless a positive timeout value
-     *              is configured, inwhich case the call returns after timeout.
-     *
-     *  @remarks    This function is passed a yieldContext, and will relinquish
-     *              the processor while it waits for the HDVICP2 to finish the
-     *              processing and return from wait.
-     *
-     *  @returns    XDAS_TRUE       Success
-     *  @returns    XDAS_FALSE      Failure/Timeout
-     */
+/**
+ *  @brief      Wait until the HDVICP2 is done processing and then return.
+ *
+ *  @remarks    This is a blocking call, unless a positive timeout value
+ *              is configured, inwhich case the call returns after timeout.
+ *
+ *  @remarks    This function is passed a yieldContext, and will relinquish
+ *              the processor while it waits for the HDVICP2 to finish the
+ *              processing and return from wait.
+ *
+ *  @returns    XDAS_TRUE       Success
+ *  @returns    XDAS_FALSE      Failure/Timeout
+ */
     XDAS_UInt32 (*wait)(IALG_Handle algHandle,
-                        struct IRES_HDVICP2_Obj *hdvicpHandle,
-                        IRES_YieldContext *yieldCtxt);
+            struct IRES_HDVICP2_Obj *hdvicpHandle,
+            IRES_YieldContext * yieldCtxt);
 
-    /**
-     *  @brief      Processing has completed.
-     *
-     *  @remarks    This is called from within the interrupt handler function when
-     *              processing completes, and is used to unblock the wait() API.
-     *
-     *  @remarks    Typically, this is called from within the algortihm's callback
-     *              function (see acquire() call).
-     */
-    XDAS_Void (*done)(IALG_Handle algHandle,
-                      struct IRES_HDVICP2_Obj *hdvicpHandle);
-    /**
-     *  @brief      Reset the HDVICP2.
-     *
-     *  @remarks    This can be called by the algorithm to "reset" the HDVICP2, and
-     *              is usually called when an acquired HDVICP2 is "dirty", i.e, it
-     *              requires both program/data reload.
-     *
-     *  @remarks    It works off a configured maximum reset cycle delay.
-     *
-     *  @returns    XDAS_TRUE       Success
-     *  @returns    XDAS_FALSE      Failure
-     */
+/**
+ *  @brief      Processing has completed.
+ *
+ *  @remarks    This is called from within the interrupt handler function when
+ *              processing completes, and is used to unblock the wait() API.
+ *
+ *  @remarks    Typically, this is called from within the algortihm's callback
+ *              function (see acquire() call).
+ */
+    XDAS_Void (*done) (IALG_Handle algHandle,
+            struct IRES_HDVICP2_Obj *hdvicpHandle);
+/**
+ *  @brief      Reset the HDVICP2.
+ *
+ *  @remarks    This can be called by the algorithm to "reset" the HDVICP2, and
+ *              is usually called when an acquired HDVICP2 is "dirty", i.e, it
+ *              requires both program/data reload.
+ *
+ *  @remarks    It works off a configured maximum reset cycle delay.
+ *
+ *  @returns    XDAS_TRUE       Success
+ *  @returns    XDAS_FALSE      Failure
+ */
     XDAS_UInt32 (*reset)(IALG_Handle algHandle,
-                         struct IRES_HDVICP2_Obj *hdvicpHandle);
+            struct IRES_HDVICP2_Obj *hdvicpHandle);
 
 } IRES_HDVICP2_Obj;
 
@@ -356,15 +351,15 @@ typedef struct IRES_HDVICP2_RegisterLayer {
  */
 typedef struct IRES_HDVICP2_Properties {
 
-    int                         size;
+    int size;
     IRES_HDVICP2_RegisterLayer *globalRegs;      /* Register layer of HDVICP2 */
 } IRES_HDVICP2_Properties;
 
 typedef IRES_Status (*ti_sdo_fc_ires_hdvicp_HDVICP2_GetHdvicp2Fxn)
-    (XDAS_Int32 *Id, IALG_Handle alg, IRES_HDVICP2_Handle handle);
+        (XDAS_Int32 *Id, IALG_Handle alg, IRES_HDVICP2_Handle handle);
 
 typedef IRES_Status (*ti_sdo_fc_ires_hdvicp_HDVICP2_FreeHdvicp2Fxn)
-    (XDAS_Int32 Id, IALG_Handle alg, IRES_HDVICP2_Handle handle);
+       (XDAS_Int32 Id, IALG_Handle alg, IRES_HDVICP2_Handle handle);
 
 #ifdef __cplusplus
 }
@@ -373,8 +368,3 @@ typedef IRES_Status (*ti_sdo_fc_ires_hdvicp_HDVICP2_FreeHdvicp2Fxn)
 /*@}*/
 
 #endif
-/*
- *  @(#) ti.sdo.fc.ires.hdvicp; 1, 0, 1,1; 6-12-2013 19:57:33; /db/atree/library/trees/fc/fc-t09/src/ xlibrary
-
- */
-
